6768d4f8de9f23cfc28052632ddfaf74e052cca6
Lukasz <lukasz.pietrzyk1991@gmail.com>
2020-12-14 10:54:57 +0100

    Add db migrations and db seed on host build

--- API/Program.cs
+++ API/Program.cs
@@ -2,25 +2,47 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Infrastructure.Data;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
 namespace API
 {
-    public class Program
+  public class Program
+  {
+    public static async Task Main(string[] args)
     {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+      var host = CreateHostBuilder(args).Build();
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+      using (var scope = host.Services.CreateScope())
+      {
+          var services = scope.ServiceProvider;
+          var loggerFactory = services.GetRequiredService<ILoggerFactory>();
+          try
+          {
+              var context = services.GetRequiredService<LoanFacilityContext>();
+              await context.Database.MigrateAsync();
+              await LoanFacilitySeed.SeedAsync(context, loggerFactory);
+          }
+          catch (Exception ex)
+          {
+              var logger = loggerFactory.CreateLogger<Program>();
+              logger.LogError(ex, "An error occurs during database migration");
+          }
+      }
+
+      host.Run();
     }
+
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+              webBuilder.UseStartup<Startup>();
+            });
+  }
 }
---
+++ Infrastructure/Data/LoanFacilitySeed.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Core.Entities;
+using Microsoft.Extensions.Logging;
+
+namespace Infrastructure.Data
+{
+  public class LoanFacilitySeed
+  {
+    public static async Task SeedAsync(LoanFacilityContext context, ILoggerFactory loggerFactory)
+    {
+      try
+      {
+        if (!context.Rates.Any())
+        {
+          var ratesData = File.ReadAllText("../Infrastructure/Data/SeedData/Rates.json");
+          var rates = JsonSerializer.Deserialize<List<Rate>>(ratesData);
+
+          foreach (var item in rates)
+          {
+            context.Rates.Add(item);
+          }
+
+          await context.SaveChangesAsync();
+        }
+
+        if (!context.Loans.Any())
+        {
+          var loansData = File.ReadAllText("../Infrastructure/Data/SeedData/Loans.json");
+          var loans = JsonSerializer.Deserialize<List<Loan>>(loansData);
+
+          foreach (var item in loans)
+          {
+            context.Loans.Add(item);
+          }
+
+          await context.SaveChangesAsync();
+        }
+      }
+      catch (Exception ex)
+      {
+        var logger = loggerFactory.CreateLogger<LoanFacilityContext>();
+        logger.LogError(ex.Message);
+      }
+    }
+  }
+}
