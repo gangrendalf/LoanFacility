f8269c18322a000f95f81fa8e51c0f0af9ed5eb2
Lukasz <lukasz.pietrzyk1991@gmail.com>
2020-12-15 12:21:55 +0100

    add navigation bar

--- client/src/app/core/components/nav-bar/nav-bar.component.ts
+++ client/src/app/core/components/nav-bar/nav-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-nav-bar',
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavBarComponent implements OnInit {
 
+  @ViewChild("menu", {static: true}) menu: ElementRef;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  toogleMenu() {
+    (this.menu.nativeElement as HTMLDivElement).classList.toggle("show");
   }
 
 }
