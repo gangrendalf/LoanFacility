51316322fed7b12734a9a5cc8cd636b91510cab6
Lukasz <lukasz.pietrzyk1991@gmail.com>
2020-12-16 16:28:21 +0100

    add test suite at loan facility service (client)

--- client/src/app/loan-facility/services/loan-facility.service.spec.ts
+++ client/src/app/loan-facility/services/loan-facility.service.spec.ts
@@ -1,12 +1,100 @@
-import { TestBed } from '@angular/core/testing';
+import { HttpClient } from '@angular/common/http';
+import { async, TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
+import { delay } from 'rxjs/operators';
+import { ILoan } from 'src/app/shared/models/loan';
+import { ILoanApplication } from 'src/app/shared/models/loan-application';
+import { ILoanOffer } from 'src/app/shared/models/loan-offer';
+import { ISchedule } from 'src/app/shared/models/schedule';
 
 import { LoanFacilityService } from './loan-facility.service';
 
-describe('LoanFacilityService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+fdescribe('LoanFacilityService', () => {
+  const testOffers: ILoanOffer[] = [
+    {description: 'test description 1', title:'test title 1', imgUrl:'test image 1', searchString: 'test string 1', id: 1},
+    {description: 'test description 2', title:'test title 2', imgUrl:'test image 2', searchString: 'test string 2', id: 2},
+    {description: 'test description 3', title:'test title 3', imgUrl:'test image 3', searchString: 'test string 3', id: 3}
+  ];
+
+  const testLoan: ILoan = { 
+    interestPerYear: 1, 
+    maxAmount: 1, 
+    maxDurationInMonths: 1, 
+    minAmount: 1, 
+    minDurationInMonths: 1, 
+    name: 'test loan 1' 
+  };
+
+  const testApplication: ILoanApplication = {
+    amount: 1, 
+    annualInterest: 1, 
+    durationInMonths: 1, 
+    payoffsPerYear: 1}
+
+  const testSchedule: ISchedule = {
+    application: testApplication,
+    title: 'test title 1',
+    schedule: null,
+    totalInterestToPay: 123,
+    totalToPay: 123
+  }
+  
+  let httpClientSpy: { get: jasmine.Spy };
+
+  let service: LoanFacilityService;
+
+  beforeEach(async(() => {
+    httpClientSpy = jasmine.createSpyObj('HttpClient', ['get']);
+
+    TestBed.configureTestingModule({
+      providers: [
+        { provide: HttpClient, useValue: httpClientSpy}
+      ]
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    service = TestBed.get(LoanFacilityService);
+
+  });
 
   it('should be created', () => {
-    const service: LoanFacilityService = TestBed.get(LoanFacilityService);
     expect(service).toBeTruthy();
   });
+
+  it(`should return offers`, (done) => {
+    httpClientSpy.get.and.returnValue( of<ILoanOffer[]>(testOffers) );
+
+    service.getOffers().subscribe(response => {
+      const expectation = JSON.stringify(testOffers);
+      const result = JSON.stringify(response);
+
+      expect(result).toEqual(expectation);
+      done();
+    });
+  });
+
+  it(`should return loans`, (done) => {
+    httpClientSpy.get.and.returnValue( of<ILoan>(testLoan) );
+
+    service.getLoan('test1').subscribe(response => {
+      const expectation = JSON.stringify(testLoan);
+      const result = JSON.stringify(response);
+      
+      expect(result).toEqual(expectation);
+      done();
+    })
+  });
+
+  it(`should return schedule`, (done) => {
+    httpClientSpy.get.and.returnValue( of<any>({body: testSchedule}) );
+
+    service.getPaybackSchedule(testApplication).subscribe(response => {
+      const expectation = JSON.stringify(testSchedule);
+      const result = JSON.stringify(response);
+
+      expect(result).toEqual(expectation);
+      done();
+    })
+  })
 });
--- client/src/app/loan-facility/services/loan-facility.service.ts
+++ client/src/app/loan-facility/services/loan-facility.service.ts
@@ -24,7 +24,6 @@ export class LoanFacilityService {
     return this.http.get<ILoanOffer[]>(this.baseUrl + 'offers').pipe(
       map(response => {
         this.offers = response;
-        
         return response;
       })
     );
